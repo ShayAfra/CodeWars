8e7ddb3ad52a8a2bc3ab6c6e7b4ff7c79291fb2f
Shay Afra <shayafra@gmail.com>
2023-01-19 18:38:25 -0800

    added reversedString.js

---
+++ reversedString.js
@@ -0,0 +1,10 @@
+// Complete the solution so that it reverses the string passed into it.
+
+// 'world'  =>  'dlrow'
+// 'word'   =>  'drow'
+function solution(str){
+    let split = str.split('')
+    let reversed = split.reverse()
+    let joined = reversed.join('')
+    return joined
+  }
